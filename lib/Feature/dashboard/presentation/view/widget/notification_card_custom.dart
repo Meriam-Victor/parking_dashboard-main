fe642cd9f7a55164203bd213c874b68d33f27a7d
Meriam Victor <meriamvictor2001@gmail.com>
2024-11-03 21:58:59 +0200

    added project files

--- lib/Feature/dashboard/presentation/view/widget/notification_card_custom.dart
+++ lib/Feature/dashboard/presentation/view/widget/notification_card_custom.dart
@@ -1,13 +1,9 @@
-import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:parking_dashboard/Core/unit/color_data.dart';
 import 'package:parking_dashboard/Core/unit/size_data.dart';
-import 'package:parking_dashboard/Core/translations/locale_keys.g.dart';
 import 'package:parking_dashboard/Core/unit/unit.dart';
-import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'package:parking_dashboard/Core/unit/assets_data.dart';
-import 'package:parking_dashboard/Core/unit/font_weight_data.dart';
 import 'package:parking_dashboard/Core/unit/style_data.dart';
 
 
